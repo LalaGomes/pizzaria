fe64ab2ddfb45ce84a07c18445a87f94b019f1b4
Lara Lays <lara.lays.bh6@gmail.com>
2022-03-07 12:49:48 -0300

    Fase 2 e 3 completa

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 const express = require('express');
+
+const app = express();
+
+app.get('/', (req,res)=>{
+    res.send("Olá visitante")
+});
+
+app.listen(3000, function(){
+    console.log('Servidor rodando na porta 3000!')
+})
