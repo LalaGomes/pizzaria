bcee99ae46b3619dc8d9cddb8a0c1efb249089b1
Lara Lays <lara.lays.bh6@gmail.com>
2022-03-07 13:10:59 -0300

    Fase 4 concluída

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 
+const PizzasRouter = require('./routers/PizzasRouter');
+
 const app = express();
 
 app.get('/', (req,res)=>{
     res.send("Olá visitante")
 });
+
+app.use('/', PizzasRouter)
 
 app.listen(3000, function(){
     console.log('Servidor rodando na porta 3000!')
---
+++ controllers/PizzasController.js
@@ -0,0 +1,3 @@
+const PizzasController = {};
+
+module.exports = PizzasController;
