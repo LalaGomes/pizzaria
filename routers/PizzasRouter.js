b16ec8220ebebdf51aa44b7cee861ba85b6a07dd
Lara Lays <lara.lays.bh6@gmail.com>
2022-03-10 23:28:31 -0300

    Um passo para a vitória, faze 14 complete

--- routers/PizzasRouter.js
+++ routers/PizzasRouter.js
@@ -6,6 +6,8 @@ const PizzasController = require('../controllers/PizzasController');
 
 router.get('/', PizzasController.listar);
 
+router.get('/pizzas/busca', PizzasController.buscar);
+
 router.get('/pizzas/:id', PizzasController.mostrar);
 
 module.exports = router;
